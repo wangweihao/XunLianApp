41413e3f82a12807865ffb37201c6c20b338f81c
wangweihao <578867817@qq.com>
2015-08-06 20:36:45 +0800

    增加注册

--- Xl_sql/ParseJson.java
+++ Xl_sql/ParseJson.java
@@ -21,14 +21,15 @@ public class ParseJson {
         System.out.println("mark:" + mark);
         /* 结果 */
         String result = null;
+        int iret = 0;
 
         switch (mark){
             /* mark == 1 检测此帐号是否正确且被注册过 */
             case 1:
                 /* 获得帐号 */
-                String account = js.getString("PhoneNumber");
+                String account1 = js.getString("PhoneNumber");
                 /* 得到获得帐号的mysql查询语句 */
-                String sql1 = "select count(*) from UserInfo where account = \"" + account + "\";";
+                String sql1 = "select count(*) from UserInfo where account = \"" + account1 + "\";";
                 /* 获得查询结果 */
                 ExecuteSql es1 = new ExecuteSql(sql1, 1);
                 result = es1.execute();
@@ -42,11 +43,20 @@ public class ParseJson {
                 }
                 break;
 
-            case 2:
-
-                String sql2 = "";
-                ExecuteSql es2 = new ExecuteSql(sql2, 2);
-                result = es2.execute();
+            case 15:
+                /* 在数据库中设置帐号和密码 */
+                String account15 = js.getString("PhoneNumber");
+                String password15 = js.getString("password");
+                String sql15 = "insert into UserInfo (account, password) values (\"" +
+                        account15 + "\",\"" + password15 + "\");";
+                ExecuteSql es15 = new ExecuteSql(sql15, 2);
+                iret = es15.update();
+                result = Integer.valueOf(iret).toString();
+                if(iret == 1){
+                    result += " 注册成功";
+                }else{
+                    result += " 注册失败";
+                }
                 break;
         }
         return result;
